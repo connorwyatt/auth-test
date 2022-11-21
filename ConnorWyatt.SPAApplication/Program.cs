4ed01c9021e853d5f732e2893d4b78ee76ae919f
Connor Wyatt <connorwyatt1@gmail.com>
2022-11-21 11:35:24 +0000

    Add SPA project

---
+++ ConnorWyatt.SPAApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var environment = builder.Environment;
+
+builder.Services.AddControllers();
+
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+  configuration.RootPath = "../app/build";
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseSpaStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+app.UseSpa(
+  spa =>
+  {
+    spa.Options.SourcePath = "../app";
+
+    if (environment.IsDevelopment())
+    {
+      spa.UseReactDevelopmentServer(npmScript: "start");
+    }
+  });
+
+app.Run();
